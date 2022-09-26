8f0ffa7037590e44bf6c1d98b07065c51fc6c552
antskos <antskos@gmail.com>
2022-09-26 12:18:54 +0300

    Добавьте файлы проекта.

---
+++ BudgetBook/Data/BudgetBookService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetBook.Data
+{
+    public class BudgetBookService
+    {
+
+        private static List<Entry> mockDb = new List<Entry>() 
+        {
+            new Entry { Description = "Test1", Amount = 4.99m },
+            new Entry { Description = "Test2", Amount = 3.05m }
+        };
+
+        public async Task<bool> AddEntry(Entry entry) 
+        {
+            mockDb.Add(entry);
+            return true;
+        }
+
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return mockDb;
+        }
+    }
+}
---
+++ BudgetBook/Data/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetBook.Data
+{
+    public class Entry
+    {
+        public decimal Amount { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
